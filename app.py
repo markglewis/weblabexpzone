65078ee90dfac584eac622f9511126809bacd45b
Mark Lewis <markglewis@gmail.com>
2020-09-04 11:28:01 -0400

    added some more btns

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ import json
 
 app = Flask(__name__)
 
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404
+
 @app.route('/soundboard', methods=['GET', 'POST'])
 def index():
 	return render_template("soundboard.html")
-'''
-@app.route('/return-file')
-def return_file(filename):
-    return send_file()
-'''

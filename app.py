b5139da3966028448601acc79eb20d57951e8633
Mark Lewis <markglewis@gmail.com>
2020-09-04 18:05:46 -0400

    index and scream changes

--- app.py
+++ app.py
@@ -8,6 +8,11 @@ def page_not_found(e):
     # note that we set the 404 status explicitly
     return render_template('404.html'), 404
 
-@app.route('/soundboard', methods=['GET', 'POST'])
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-	return render_template("soundboard.html")
+	return render_template("index.html")
+
+@app.route('/soundboard', methods=['GET', 'POST'])
+def soundboard():
+	return render_template('soundboard.html')
